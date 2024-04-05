4657d06040912a80e94022ff241b3e3016781892
dzl71 <davidzlatin406@gmail.com>
2024-04-05 23:41:38 +0300

    added constants for raycasting, and changed the map size

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,9 @@ import math
 WIDTH: int = 1600
 HEIGHT: int = 900
 RESOLUTION: tuple[int, int] = (WIDTH, HEIGHT)
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+HALF_RESOLUTION = (HALF_WIDTH, HALF_HEIGHT)
 
 FPS: int = 60
 
@@ -20,6 +23,18 @@ MAP: list[list[int]] = [
     [1, _, _, _, _, 1, 1, 1, 1, 1, 1, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
@@ -28,9 +43,13 @@ PLAYER_ANGLE: float = 0
 PLAYER_MOVEMENT_SPEED: float = 0.004
 PLAYER_ROTATION_SPEED: float = 0.002
 
-FIELD_OF_VIEW = math.pi / 3 / 3
+FIELD_OF_VIEW = math.pi / 3
 HALF_FOV = FIELD_OF_VIEW / 2
-RAYS_NUM = WIDTH // 50
+RAYS_NUM = WIDTH // 2  # more rays == smoother walls
 HALF_RAYS_NUM = RAYS_NUM // 2
 DELTA_ANGLE = FIELD_OF_VIEW / RAYS_NUM
 MAX_DEPTH = 30
+
+SCREEN_DISTANCE = HALF_WIDTH / math.tan(HALF_FOV)
+WALL_HEIGHT = 1
+SCALE = WIDTH // RAYS_NUM
