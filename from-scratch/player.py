93bb748ec82442b5ed140f4335e2495cbe7a655c
dzl71 <davidzlatin406@gmail.com>
2024-04-09 23:26:31 +0300

    made a base for the player (needed for raycasting)

---
+++ from-scratch/player.py
@@ -0,0 +1,9 @@
+import constants as const
+
+
+class Player:
+    def __init__(self) -> None:
+        # self.game = game
+        self.angle = const.STARTING_ANGLE
+        self.x = 8
+        self.y = 1
