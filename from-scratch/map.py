c599e92a0698c2193821e22c3c56cee5038f634f
dzl71 <davidzlatin406@gmail.com>
2024-04-11 22:04:54 +0300

    added a field that contains wall positions

--- from-scratch/map.py
+++ from-scratch/map.py
@@ -5,11 +5,22 @@ import pygame as pg
 class Map:
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.minimap = const.MAP
+        self.map = const.MAP
+        self.wall_positions = self.get_wall_positoins()
 
     def get_wall_positoins(self) -> dict[tuple[int, int], int]:
         wall_positoins: dict[tuple[int, int], int] = {}
-        for row_idx, row in enumerate(self.minimap):
+        for row_idx, row in enumerate(self.map):
             for tile_idx, tile in enumerate(row):
-                wall_positoins[(row_idx, tile_idx)] = tile
+                if tile > 0:
+                    wall_positoins[(row_idx, tile_idx)] = tile
         return wall_positoins
+
+    def draw(self) -> None:
+        for pos in self.get_wall_positoins().keys():
+            pg.draw.rect(
+                self.screen,
+                'darkgray',
+                (pos[1] * 100, pos[0] * 100, 100, 100),
+                2,
+            )
