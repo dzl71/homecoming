8157477bb7154d2e76fba806cf7e3abbc62cb780
dzl71 <davidzlatin406@gmail.com>
2024-04-11 22:06:17 +0300

    added raycasting method

--- from-scratch/raycasting.py
+++ from-scratch/raycasting.py
@@ -1,13 +1,14 @@
+import math
+import pygame as pg
 import constants as const
 from map import Map
-import math
 
 
 class RayCasting:
-    def __init__(self, player) -> None:
+    def __init__(self, game) -> None:
         # self.game = game
-        self.map = Map(2)  # placeholder value
-        self.player = player
+        self.map = game.map  # placeholder value
+        self.player = game.player
 
     def vert_wall_distance(self, ray_angle: float) -> float:
         '''
@@ -20,44 +21,41 @@ class RayCasting:
         '''
         # caching repeated calculations to decrese calculation amount
         # adding a small value to the angle to avoid division by zero
-        mod_ray_angle: float = ray_angle + 1e-6
-        rad_angle: float = math.radians(mod_ray_angle)
+        rad_angle: float = math.radians(ray_angle)
         cos_a: float = math.cos(rad_angle)
-        tan_a: float = math.tan(rad_angle)
+        sin_a: float = math.sin(rad_angle)
 
         # calculating the x of first intersection and the delta_x of the following intersections
-        delta_x: float
         intersect_x: int
-        if 0 <= mod_ray_angle % 360 < 90 or 270 < mod_ray_angle % 360:
+        delta_x: float
+        if 0 <= ray_angle % 360 < 90 or 270 < ray_angle % 360:
             delta_x = 1
             intersect_x = math.ceil(self.player.x)
         else:
             delta_x = -1
-            intersect_x = math.floor(self.player.x)
-        print(f"inital {intersect_x = }")
+            intersect_x = math.floor(self.player.x) - 1e-6
 
         # calculating the depth of first intersection and the delta_depth of the following intersections
-        depth: float = abs(intersect_x - self.player.x) / cos_a
-        print(f"inital {depth = }")
+        depth: float = (intersect_x - self.player.x) / cos_a
         delta_depth: float = delta_x / cos_a
 
         # calculating the y of first intersection and the delta_y of the following intersections
-        delta_y: float = delta_x * tan_a
-        intersect_y: float
-        if 0 <= mod_ray_angle % 360 < 180:
-            intersect_y = self.player.y + tan_a * intersect_x
-        else:
-            intersect_y = self.player.y - tan_a * intersect_x
-        print(f"inital {intersect_y = }")
+        delta_y: float = delta_depth * sin_a
+        intersect_y: float = self.player.y + depth * sin_a
+
+        if ray_angle == self.player.angle - const.HALF_FOV + 1e-6:
+            print(f"initial_vert_{intersect_x = }, initial_vert_{intersect_y = }")
 
         # sending the ray deeper until collision accures, or exiting the bounds
-        while depth < const.MAX_RAY_DEPTH:
+        for _ in range(const.MAX_RAY_DEPTH):
+            if (int(intersect_y), int(intersect_x)) in self.map.wall_positions:
+                break
             intersect_x += delta_x
             intersect_y += delta_y
             depth += delta_depth
-            print(f"(vert)  {intersect_x = }, {intersect_y = }, {depth = }\n")
-            if (intersect_y, intersect_x) not in self.map.get_wall_positoins():
-                break
+
+        if ray_angle == self.player.angle - const.HALF_FOV + 1e-6:
+            print(f"vert_{intersect_x = }, vert_{intersect_y = }\n")
 
         return depth
 
@@ -72,43 +70,75 @@ class RayCasting:
         '''
         # caching repeated calculations to decrese calculation amount
         # adding a small value to the angle to avoid division by zero
-        mod_ray_angle: float = ray_angle + 1e-6
-        rad_angle: float = math.radians(mod_ray_angle)
+        rad_angle: float = math.radians(ray_angle)
         sin_a: float = math.sin(rad_angle)
-        tan_a: float = math.tan(rad_angle)
+        cos_a: float = math.cos(rad_angle)
 
         # calculating the y of first intersection and the delta_y of the following intersections
         delta_y: float
         intersect_y: int
-        if 0 <= mod_ray_angle % 360 < 180:
+        if 0 < ray_angle % 360 < 180:
             delta_y = 1
             intersect_y = math.ceil(self.player.y)
         else:
             delta_y = -1
-            intersect_y = math.floor(self.player.y)
-        print(f"initial {intersect_y = }")
+            intersect_y = math.floor(self.player.y) - 1e-6
 
         # calculating the depth of first intersection and the delta_depth of the following intersections
-        depth: float = abs(intersect_y - self.player.y) / sin_a
-        print(f"inital {depth = }")
+        depth: float = (intersect_y - self.player.y) / sin_a
         delta_depth: float = delta_y / sin_a
-        delta_x: float = delta_y / tan_a
 
         # calculating the x of first intersection and the delta_x of the following intersections
-        intersect_x: float
-        if 0 <= mod_ray_angle % 360 < 90 or 270 < mod_ray_angle % 360:
-            intersect_x = self.player.x + intersect_y / tan_a
-        else:
-            intersect_x = self.player.x - intersect_y / tan_a
-        print(f"inital {intersect_x = }")
+        delta_x: float = delta_depth * cos_a
+        intersect_x: float = self.player.x + depth * cos_a
+
+        if ray_angle == self.player.angle - const.HALF_FOV + 1e-6:
+            print(f"initial_hor_{intersect_x = }, initial_hor_{intersect_y = }")
 
         # sending the ray deeper until collision accures, or exiting the bounds
-        while depth < const.MAX_RAY_DEPTH:
+        for _ in range(const.MAX_RAY_DEPTH):
+            if (int(intersect_y), int(intersect_x)) in self.map.wall_positions:
+                break
             intersect_x += delta_x
             intersect_y += delta_y
             depth += delta_depth
-            print(f"(hor)  {intersect_x = }, {intersect_y = }, {depth = }\n")
-            if (intersect_y, intersect_x) not in self.map.get_wall_positoins():
-                break
+
+        if ray_angle == self.player.angle - const.HALF_FOV + 1e-6:
+            print(f"hor_{intersect_x = }, hor_{intersect_y = }\n")
 
         return depth
+
+    def ray_cast(self, screen) -> None:
+        ray_angle: float = self.player.angle - const.HALF_FOV + 1e-6
+        for ray in range(const.RAY_NUM):
+            ray_depth: float = min(
+                self.vert_wall_distance(ray_angle),
+                self.hor_wall_distance(ray_angle)
+            )
+            if ray_angle == self.player.angle - const.HALF_FOV + 1e-6:
+                print(f"{math.radians(ray_angle) = }")
+                print(f"{self.vert_wall_distance(ray_angle) = }")
+                print(f"{self.hor_wall_distance(ray_angle) = }")
+                print(f"{ray_depth = }")
+                print("\n====================================================\n")
+
+            # remove the fishblow effect
+            ray_depth *= math.cos(math.radians(self.player.angle - ray_angle))
+
+            # projection height
+            proj_height = const.SCREEN_DISTANCE / (ray_depth + 1e-6)
+
+            # draw walls
+            # color = [255 / (1 + depth ** 5 * 2e-5)] * 3
+            pg.draw.rect(
+                screen,
+                'white',
+                (
+                    ray * const.SCALE,
+                    const.HALF_HEIGHT - proj_height // 2,
+                    const.SCALE,
+                    proj_height
+                ),
+            )
+
+            ray_angle += const.DELTA_ANGLE
