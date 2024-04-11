e1981742b96db72468d920790823c48c6717654b
dzl71 <davidzlatin406@gmail.com>
2024-04-11 22:03:54 +0300

    added some important constants

--- from-scratch/constants.py
+++ from-scratch/constants.py
@@ -1,6 +1,11 @@
+import math
+
 WIDTH: int = 1600
+HALF_WIDTH: int = WIDTH // 2
 HEIGHT: int = 900
+HALF_HEIGHT: int = HEIGHT // 2
 RESOLUTION: tuple[int, int] = (WIDTH, HEIGHT)
+
 
 FPS: int = 60
 
@@ -28,5 +33,19 @@ MAP = [
 ]
 
 STARTING_ANGLE = 0
+STARTING_X = 1
+STARTING_Y = 1
 
 MAX_RAY_DEPTH = 16  # recommended to be the smallest dimention of the map
+FOV = 60  # in degrees
+HALF_FOV = FOV / 2
+RAY_NUM = WIDTH // 2 // 3
+HALF_RAY_NUM = RAY_NUM // 2
+DELTA_ANGLE = FOV / RAY_NUM
+
+PLAYER_MOVEMENT_SPEED: float = 0.004
+PLAYER_ROTATION_SPEED: float = 0.15
+
+SCREEN_DISTANCE = HALF_WIDTH / math.tan(math.radians(HALF_FOV))
+WALL_HEIGHT = 1
+SCALE = WIDTH // RAY_NUM
