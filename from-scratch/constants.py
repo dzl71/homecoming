eea32173b01ebff3296f4d45868acac891d946c8
dzl71 <davidzlatin406@gmail.com>
2024-04-13 06:40:28 +0300

    changed some constants to radians

--- from-scratch/constants.py
+++ from-scratch/constants.py
@@ -2,7 +2,7 @@ import math
 
 WIDTH: int = 1600
 HALF_WIDTH: int = WIDTH // 2
-HEIGHT: int = 900
+HEIGHT: int =  900
 HALF_HEIGHT: int = HEIGHT // 2
 RESOLUTION: tuple[int, int] = (WIDTH, HEIGHT)
 
@@ -19,11 +19,11 @@ MAP = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -33,19 +33,23 @@ MAP = [
 ]
 
 STARTING_ANGLE = 0
-STARTING_X = 1
-STARTING_Y = 1
+STARTING_X = 5
+STARTING_Y = 5
+PLAYER_SIZE_SCALE = 30
 
-MAX_RAY_DEPTH = 16  # recommended to be the smallest dimention of the map
-FOV = 60  # in degrees
+MAX_RAY_DEPTH = 3  # recommended to be the smallest dimention of the map
+FOV = math.pi / 3  # in radians
 HALF_FOV = FOV / 2
-RAY_NUM = WIDTH // 2 // 3
+RAY_NUM = WIDTH // 5
 HALF_RAY_NUM = RAY_NUM // 2
 DELTA_ANGLE = FOV / RAY_NUM
 
 PLAYER_MOVEMENT_SPEED: float = 0.004
-PLAYER_ROTATION_SPEED: float = 0.15
+PLAYER_ROTATION_SPEED: float = 0.001
 
-SCREEN_DISTANCE = HALF_WIDTH / math.tan(math.radians(HALF_FOV))
+SCREEN_DISTANCE = HALF_WIDTH / math.tan(HALF_FOV)
 WALL_HEIGHT = 1
 SCALE = WIDTH // RAY_NUM
+
+TEXTURE_SIZE: int = 255
+HALF_TEXTURE_SIZE: int = TEXTURE_SIZE // 2
