168ea4544ced20da8c7c460774f0e9ec58567509
dzl71 <davidzlatin406@gmail.com>
2024-04-14 22:37:36 +0300

    removed displaying player

--- src/player.py
+++ src/player.py
@@ -21,39 +21,39 @@ class Player:
         speed_cos = speed * cos_a
 
         keys = pg.key.get_pressed()
-        if keys[pg.K_w] and not self.game.displaying_map:
+        if keys[pg.K_w] and not self.game.options.displaying_map:
             dx += speed_cos
             dy += speed_sin
-        if keys[pg.K_s] and not self.game.displaying_map:
+        if keys[pg.K_s] and not self.game.options.displaying_map:
             dx -= speed_cos
             dy -= speed_sin
-        if keys[pg.K_a] and not self.game.displaying_map:
+        if keys[pg.K_a] and not self.game.options.displaying_map:
             dx += speed_sin
             dy -= speed_cos
-        if keys[pg.K_d] and not self.game.displaying_map:
+        if keys[pg.K_d] and not self.game.options.displaying_map:
             dx -= speed_sin
             dy += speed_cos
 
         self.check_wall_collision(dx, dy)
 
-        if keys[pg.K_LEFT] and not self.game.displaying_map:
+        if keys[pg.K_LEFT] and not self.game.options.displaying_map:
             self.angle -= const.PLAYER_ROTATION_SPEED * self.game.delta_time
 
-        if keys[pg.K_RIGHT] and not self.game.displaying_map:
+        if keys[pg.K_RIGHT] and not self.game.options.displaying_map:
             self.angle += const.PLAYER_ROTATION_SPEED * self.game.delta_time
 
         self.angle %= math.tau
 
         if keys[pg.K_m]:
-            if not self.game.displaying_map:
-                self.game.raycast = False
+            if not self.game.options.displaying_map and self.game.options.map_usages > 0:
+                self.game.options.displaying_map = True
+                self.game.options.map_usages -= 1
             else:
-                self.game.raycast = True
-            self.game.displaying_map = not self.game.displaying_map
+                self.game.options.displaying_map = False
             time.sleep(0.08)
 
     def check_wall(self, x: int, y: int) -> bool:
-        return (y, x) not in self.game.map.wall_positions
+        return (x, y) not in self.game.map.wall_positions
 
     def check_wall_collision(self, dx: float, dy: float) -> None:
         scale = const.PLAYER_SIZE_SCALE / self.game.delta_time
@@ -61,51 +61,6 @@ class Player:
             self.x += dx
         if self.check_wall(int(self.x), int(self.y + dy * scale)):
             self.y += dy
-
-    # test funtion
-    def draw(self) -> None:
-        pg.draw.line(
-            self.game.screen,
-            'yellow',
-            (self.x * 100, self.y * 100),
-            (
-                self.x * 100 + const.WIDTH * math.cos(self.angle),
-                self.y * 100 + const.WIDTH * math.sin(self.angle)
-            ),
-            2,
-        )
-        # draw field of view
-        # for i in range(1, const.RAY_NUM // 2 + 1):
-        #     pg.draw.line(
-        #         self.game.screen,
-        #         'darkgray',
-        #         (self.x * 100, self.y * 100),
-        #         (
-        #             self.x * 100 + const.WIDTH *
-        #             math.cos(self.angle + const.DELTA_ANGLE * i),
-        #             self.y * 100 + const.WIDTH *
-        #             math.sin(self.angle + const.DELTA_ANGLE * i)
-        #         ),
-        #         2,
-        #     )
-        #     pg.draw.line(
-        #         self.game.screen,
-        #         'darkgray',
-        #         (self.x * 100, self.y * 100),
-        #         (
-        #             self.x * 100 + const.WIDTH *
-        #             math.cos(self.angle - const.DELTA_ANGLE * i),
-        #             self.y * 100 + const.WIDTH *
-        #             math.sin(self.angle - const.DELTA_ANGLE * i),
-        #         ),
-        #         2,
-        #     )
-        pg.draw.circle(
-            self.game.screen,
-            'green',
-            (self.x * 100, self.y * 100),
-            3,
-        )
 
     def update(self) -> None:
         self.movement()
