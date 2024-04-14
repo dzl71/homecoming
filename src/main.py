e59536ae74f2c623359ab687a0c24d31c65646c8
dzl71 <davidzlatin406@gmail.com>
2024-04-14 14:03:41 +0300

    raycasting separated from map_drawing

--- src/main.py
+++ src/main.py
@@ -16,7 +16,6 @@ class Game:
         self.screen: pg.surface.Surface = pg.display.set_mode(const.RESOLUTION)
         self.clock: pg.time.Clock = pg.time.Clock()
         self.delta_time: int = 1
-        self.object_renderer = ObjectRenderer(self.screen)
         self.raycast: bool = True
         self.displaying_map: bool = False
         self.new_game()
@@ -24,24 +23,24 @@ class Game:
     def new_game(self) -> None:
         self.map: Map = Map(self.screen)
         self.player: Player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting: RayCasting = RayCasting(self)
 
     def update(self) -> None:
         self.player.update()
-        if self.raycast == True:
+        if self.raycast:
             self.raycasting.ray_cast(self.screen)
         pg.display.flip()
         self.delta_time = self.clock.tick(const.FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
 
-    def draw(self) -> None:
-        self.screen.fill('black')
+    def draw_map(self) -> None:
         self.map.draw()
         self.player.draw()
 
     def check_events(self) -> None:
         for event in pg.event.get():
-            if event.type == pg.QUIT or event.type == pg.K_ESCAPE:
+            if event.type in (pg.QUIT, pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
 
@@ -49,7 +48,9 @@ class Game:
         while True:
             self.check_events()
             self.update()
-            self.draw()
+            self.screen.fill('black')
+            if self.displaying_map:
+                self.draw_map()
 
 
 if __name__ == '__main__':
