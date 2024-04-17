deb48331fbd17a10598922ae07934e897a820153
dzl71 <davidzlatin406@gmail.com>
2024-04-18 01:32:48 +0300

    refactored

--- src/options.py
+++ src/options.py
@@ -14,28 +14,19 @@ class Options:
     def display_data(self) -> None:
         font = pg.font.SysFont(const.FONT, const.FONT_SIZE)
         options = [
-            font.render(
-                f'Hostages to rescue: {self.hostages_to_rescue}',
-                False,
-                (255, 255, 255)
-            ),
-            font.render(
-                f'Time left: {self.time_left // 60}:{self.time_left % 60:0>2}',
-                False,
-                (255, 255, 255)
-            ),
-            font.render(
-                f'Map usages: {self.map_usages}',
-                False,
-                (255, 255, 255)
-            ),
-            font.render(
-                f'PathFinding usages: {self.pathfinding_usages}',
-                False,
-                (255, 255, 255)
-            ),
+            f'Hostages to rescue: {self.hostages_to_rescue}',
+            f'Time left: {self.time_left // 60}:{self.time_left % 60:0>2}',
+            f'Map usages: {self.map_usages}',
+            f'PathFinding usages: {self.pathfinding_usages}',
         ]
-        for idx, option in enumerate(options):
-            self.screen.blit(option, (3, 3 + idx * const.FONT_SIZE))
+        for idx, option_text in enumerate(options):
+            self.screen.blit(
+                font.render(
+                    option_text,
+                    False,
+                    (255, 255, 255)
+                ),
+                (3, 3 + idx * const.FONT_SIZE)
+            )
 
 # comment to commit
